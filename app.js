97afa9bceb0df6e7b55dfd260f10ef8d8c237f78
Lynnze <aunghtetlinn.yit@gmail.com>
2021-09-26 15:32:05 +0630

    fix code and contact form

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
+// removing message
+const alertForm = document.querySelector('.form-alert')
+alertForm.classList.add('alert-show');
+setTimeout(function(){
+    alertForm.classList.remove('alert-show');
+},4000);
+
+
 //  for preload
 
-const word1 = document.querySelectorAll('.preload svg path');
+// const word1 = document.querySelectorAll('.preload svg path');
 
-for(i=12;i<word1.length;i++){
-    let l =word1[i].getTotalLength();
-    word1[i].style.strokeDasharray = l;
-    word1[i].style.strokeDashoffset = l;
-    word1[i].style.animation = `write-word 3s ease forwards ${(i*0.3 )+0.3}`;
-    word1[i].style.animationIterationCount= '1';
+// for(i=12;i<word1.length;i++){
+//     let l =word1[i].getTotalLength();
+//     word1[i].style.strokeDasharray = l;
+//     word1[i].style.strokeDashoffset = l;
+//     word1[i].style.animation = `write-word 3s ease forwards ${(i*0.3 )+0.3}`;
+//     word1[i].style.animationIterationCount= '1';
 
-}
+// }
 
-word1[14].style.animation += ',word-fade 1.5s ease forwards 4s';
-word1[22].style.animation += ',word-fade 1.5s ease forwards 4s';
-word1[23].style.animation += ',word-fade 1.5s ease forwards 4s';
+// word1[14].style.animation += ',word-fade 1.5s ease forwards 4s';
+// word1[22].style.animation += ',word-fade 1.5s ease forwards 4s';
+// word1[23].style.animation += ',word-fade 1.5s ease forwards 4s';
 
 
-const preload = document.querySelector('.preload');
+// const preload = document.querySelector('.preload');
 
 
 
 const aniText = document.querySelector('.showcase-content p')
-setTimeout(function(){
-    preload.classList.add('fade-pre')
+// setTimeout(function(){
+    // preload.classList.add('fade-pre')
     document.body.classList.add('show-body')
     aniText.classList.add('text-anim')
     changeAni();
-},5000)
+// },5000)
 
 
 
@@ -132,9 +140,9 @@ function zoomText(){
     
     zText.innerText = this.innerText;
     zTextCon.classList.add('text-zoomed')
-    // setTimeout(function(){
-    //     zTextCon.classList.remove('text-zoomed')
-    // },1000)
+    setTimeout(function(){
+        zTextCon.classList.remove('text-zoomed')
+    },1000)
 }
 
 
@@ -145,6 +153,32 @@ const zoomPCon = document.querySelector('.zoom-project');
 const zoomPImg = document.querySelector('.zoom-project-img');
 
 
+//  change text of project info
+
+const proHead = document.querySelector('.zoom-project-info h3');
+const proadv = document.querySelector('.zoom-project-info ul');
+const proLinks = document.querySelectorAll('.action-link');
+
+let advArr = ['<li>Amazing sound effects</li><li>Canvas-based game</li><li>Own AI algorithm</li>','<li>Cool pictures of animals</li><li>3 Difficulty levels</li><li>100% own code</li>', '<li>Stunning hover effects</li><li>Light and dark themes</li><li>100% responsive design</li>','<li>Awesome Navigation effect</li><li>100% CSS Grid</li><li>Responsive web design</li>','<li>Amazing animations</li><li>3D experience</li><li>PHP contact form</li>','<li>Cool text effects</li><li>Awesome spinner</li><li>Custom time counter</li>']
+
+let HeadArr = ['Ping Pong','Test Your Memory', 'Gymatic fitness','Vision Luxury Villas','My Portfolio','Been Together']
+
+let linkArr = [
+['https://nandy.tech/pingpong/','https://github.com/Lynnze-24/pingpong'],
+['https://nandy.tech/Memorygame/','https://github.com/Lynnze-24/Memorygame'], 
+['https://nandy.tech/gymatic/','https://github.com/Lynnze-24/gymatic'],
+['https://nandy.tech/vlv/','https://github.com/Lynnze-24/vlv'],
+['https://nandy.tech/portfolio/','https://github.com/Lynnze-24/portfolio']
+,
+['https://nandy.tech/anni2nd/','https://github.com/Lynnze-24/beenTogether']
+]
+
+function proInfoDisplay(n){
+  proHead.innerText = HeadArr[n];
+ proadv.innerHTML = advArr[n];
+ proLinks[0].setAttribute('href',linkArr[n][0]);
+ proLinks[1].setAttribute('href',linkArr[n][1]);
+}
 
 projectSm.forEach(function(sm){
     sm.addEventListener('click',zoomProject);
@@ -159,6 +193,9 @@ function zoomProject(){
         zoomPCon.classList.remove('active-project');
          zoomPCon.classList.remove('over-ani');
     }
+    let pronum = this.getAttribute('data-img')
+    setTimeout( function(){proInfoDisplay(pronum)},500);
+   
     this.classList.add('projectsm-vanish')
     const src= this.getAttribute('src')
     addBlocks(src);
@@ -170,7 +207,8 @@ function zoomProject(){
 }
 
 function addBlocks(src){
-    
+    const checkReso = document.querySelector('.zoom-project');
+    const rectW = checkReso.getBoundingClientRect().width;
    
     let xpUp =true;
     let xp = 0;
@@ -181,7 +219,12 @@ function addBlocks(src){
         const zoomblock = document.querySelectorAll('.block');
         zoomblock[i].style.backgroundImage= `url(${src})`;
         zoomblock[i].style.backgroundPosition = `${xp}% ${yp}%`;
-        zoomblock[i].style.backgroundSize = '60rem 30rem';
+        if (rectW>400){
+            zoomblock[i].style.backgroundSize = '60rem 30rem';
+        }else{
+            zoomblock[i].style.backgroundSize = '36rem 18rem';
+        }
+        
 
         if(xpUp) xp+= 11.1;
         if(xp>100){xp=0;xpUp=false}
@@ -193,16 +236,136 @@ function addBlocks(src){
 
 const closeProBtn = document.querySelector('.zoom-project-info p');
 
-closeProBtn.addEventListener('click',function(){
-    const vanish = document.querySelector('.projectsm-vanish');
-    proZoom = false;
-    zoomPCon.classList.remove('active-project');
-    
-        zoomPCon.classList.remove('over-ani');
+closeProBtn.addEventListener('click',closeProjectBg);
 
-        setTimeout(function(){
-            zoomPImg.innerHTML = "" ;
-            vanish.classList.remove('projectsm-vanish')
-           },1000)
+window.addEventListener('resize',closeProjectBg);
+
+function closeProjectBg(){
     
-})
+        const vanish = document.querySelector('.projectsm-vanish');
+        if(vanish){
+            proZoom = false;
+            zoomPCon.classList.remove('active-project');
+            
+                zoomPCon.classList.remove('over-ani');
+        
+                setTimeout(function(){
+                    zoomPImg.innerHTML = "" ;
+                    vanish.classList.remove('projectsm-vanish')
+                   },1000)
+        }
+       
+        
+    
+}
+
+
+
+
+// browser compactability for chrome
+
+// fix hover bug in chrome
+let contactCon = document.querySelector('.contact')
+let contactWrap = document.querySelector('.contact-wrap')
+let gitCon = document.querySelector('.git')
+let linkedCon = document.querySelector('.linked')
+
+contactCon.addEventListener('mouseover',revealIcon)
+
+
+function revealIcon(event){
+//   console.log(event.target)
+    if(event.target==gitCon.children[0]){
+        gitCon.classList.add('rotate-shoe');
+        linkedCon.classList.remove('rotate-shoe')
+    }
+    if(event.target==linkedCon.children[0]){
+        gitCon.classList.remove('rotate-shoe')
+        linkedCon.classList.add('rotate-shoe')
+    }
+    if(event.target==contactWrap){
+        gitCon.classList.remove('rotate-shoe')
+        linkedCon.classList.remove('rotate-shoe')
+    }
+    
+}
+
+
+// submit form to send message
+
+
+const formInputs = document.querySelectorAll('.contact-form input');
+const messageInput = document.querySelector('.contact-form textarea');
+const emailInput = formInputs[0];
+const subjectInput = formInputs[1];
+
+const formSubBtn = document.querySelector('.contact-form button');
+
+const messageAlert = document.querySelector('.form-alert');
+
+formSubBtn.addEventListener('click',sendMessage)
+const regxEmail = /\w@gmail.com$/;
+function sendMessage(e){
+    
+    if(emailInput.value!=''){
+        e.preventDefault();
+     let isMailValid = regxEmail.test(emailInput.value);
+        if(isMailValid==false){
+            messageAlert.classList.add('alert-show')
+        messageAlert.style.backgroundColor= 'red'
+        messageAlert.innerText= 'Email Invalid!'
+        setTimeout(function(){
+            messageAlert.classList.remove('alert-show')
+        },1000)
+        return;
+        }
+        
+
+    }
+
+
+    if(emailInput.value==''){
+        e.preventDefault();
+        messageAlert.classList.add('alert-show')
+        messageAlert.style.backgroundColor= 'red'
+        messageAlert.innerText= 'Email required!'
+        setTimeout(function(){
+            messageAlert.classList.remove('alert-show')
+        },1000)
+
+    }else if(subjectInput.value==''){
+        e.preventDefault();
+        messageAlert.classList.add('alert-show')
+        messageAlert.style.backgroundColor= 'red'
+        messageAlert.innerText= 'Subject required!'
+        setTimeout(function(){
+            messageAlert.classList.remove('alert-show')
+        },1000)
+
+    }else if(messageInput.value==''){
+        e.preventDefault();
+        messageAlert.classList.add('alert-show')
+        messageAlert.style.backgroundColor= 'red'
+        messageAlert.innerText= 'Message required!'
+        setTimeout(function(){
+            messageAlert.classList.remove('alert-show')
+        },1000)
+
+    }else{
+        messageForm.submit();
+       
+        
+        //    messageAlert.classList.add('alert-show')
+        // messageAlert.style.backgroundColor= 'green'
+        // messageAlert.innerText= 'Message sent!I will contact you ASAP'
+        setTimeout(function(){
+            messageForm.submit();
+            messageAlert.classList.remove('alert-show')
+            emailInput.value='';
+            subjectInput.value='';
+            messageInput.value='';
+        },1000)
+        
+    }
+    
+}
